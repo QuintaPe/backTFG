cb6560a788d9a40e891f8145d2d164ff372afe64
Alejandro <alexquintape@gmail.com>
2023-01-10 20:00:52 +0100

    .gitignore added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGODB_HOST: process.env.MONGODB_HOST || 'lAlejandro22:cTuIUr5cQ9hqaMB7@cluster0.a5lan.mongodb.net',
+  MONGODB_HOST: process.env.MONGODB_HOST || 'lAlejandro22:kZ8cjRjrmrTeZDPA@cluster0.a5lan.mongodb.net',
   MONGODB_DATABASE: process.env.MONGODB_DB || 'ScoutCamp'
 }
